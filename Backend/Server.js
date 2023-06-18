be8f1a7cfc3ab6d4eaa3d88ccb3cd8527c55b427
farsan <apfarsan@gmail.com>
2023-06-18 12:55:53 +0530

    Update

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 4578
 const mongoose = require('mongoose');
+const cors = require("cors")
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({extended:true})); // to render form data from frontend
+
+
+
 
 
 mongoose.connect("mongodb+srv://apfarsan:SrxdUcdJqDd66GRQ@cluster0.kz71uey.mongodb.net/")
@@ -9,13 +16,20 @@ mongoose.connect("mongodb+srv://apfarsan:SrxdUcdJqDd66GRQ@cluster0.kz71uey.mongo
 .catch((err)=>{console.log(err)})
 
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
+app.post('/addData', (req, res) => {
+    try {
+
+        let item = req.body
+        console.log(item)
+
+
+
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
-})
+});
