8b664c7e35a6815cc4288362d4cd166ebbe557c0
farsan <apfarsan@gmail.com>
2023-06-18 16:03:51 +0530

    2n_dupdate

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 4578
 const mongoose = require('mongoose');
 const cors = require("cors")
+const PRODUCT = require("./model/products")
 app.use(cors())  // cor policy activation
 app.use(express.json()); // to render json req from frontend
 app.use(express.urlencoded({extended:true})); // to render form data from frontend
@@ -11,16 +12,21 @@ app.use(express.urlencoded({extended:true})); // to render form data from fronte
 
 
 
-mongoose.connect("mongodb+srv://apfarsan:SrxdUcdJqDd66GRQ@cluster0.kz71uey.mongodb.net/")
+mongoose.connect("mongodb+srv://apfarsan:SrxdUcdJqDd66GRQ@cluster0.kz71uey.mongodb.net/MES")
 .then(()=>{console.log("MongoDB connected successfully")})
 .catch((err)=>{console.log(err)})
 
 
-app.post('/addData', (req, res) => {
+app.post('/addData',async (req, res) => {
     try {
 
         let item = req.body
         console.log(item)
+
+        const saveData = await PRODUCT(item) //PRODUCT is a model. we cross check the form data with the model we created before saving it
+        await saveData.save()  // through this code we save the incoming data from front end to db 
+        res.send((saveData))
+
 
 
 
